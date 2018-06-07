c46c76358c9161d0ed3821ba5bc5333d7965dae0
hyunsuck <qu3230@gmail.com>
2018-06-07 23:52:04 +0900

    파일 해석용 주석 추가

--- test.py
+++ test.py
@@ -54,51 +54,58 @@ def setMotorContorl(pwm, INA, INB, speed, stat):
 
     #모터 속도 제어 PWM
     pwm.ChangeDutyCycle(speed)  
-    
+
+    #앞으로 A=1, B=0
     if stat == FORWARD:
         GPIO.output(INA, HIGH)
         GPIO.output(INB, LOW)
-        
-    #뒤로
+
+    #뒤로 A=0, B=1
     elif stat == BACKWORD:
         GPIO.output(INA, LOW)
         GPIO.output(INB, HIGH)
-        
-    #정지
+
+    #정지 A=0, B=0
     elif stat == STOP:
         GPIO.output(INA, LOW)
         GPIO.output(INB, LOW)
 
-        
+
 # 모터 제어함수 간단하게 사용하기 위해 한번더 래핑(감쌈)
 def setMotor(ch, speed, stat):
     if ch == CH1:
         #pwmA는 핀 설정 후 pwm 핸들을 리턴 받은 값이다.
+        #37, 35 핀의 값
         setMotorContorl(pwmA, IN1, IN2, speed, stat)
     else:
         #pwmB는 핀 설정 후 pwm 핸들을 리턴 받은 값이다.
         setMotorContorl(pwmB, IN3, IN4, speed, stat)
-  
 
-# GPIO 모드 설정 
+
+# 라즈베리와 아두이노 연결 모듈 만들기
+# GPIO 모드 설정
 GPIO.setmode(GPIO.BCM)
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz=1000000
 
+#라즈베리파이에서 아날로그 신호를 읽기 위한 함수
+#ADC를 통해 변환된 아날로그 신호를 라즈베리파이에서 SPI 통신 프로토콜을 이용하여 읽기 위해서는 라즈베리파이에서 SPI를 사용할 수 있게 해줘야 한다.
 def analog_read(channel):
+        #안에 있는 데이터를 쓰고 리턴된 값을 반환한다.
 	r = spi.xfer2([1,(8 + channel) <<  4, 0])
+        #8비트 중 1, 2번째 비트는 ADC 데이터의 9, 10번째 비트, 세번째는 하위 8비트를 의미한다.
 	adc_out = ((r[1]&3) << 8) + r[2]
 	return adc_out
 
 
 #모터 핀 설정
-#핀 설정후 PWM 핸들 얻어옴 
+#핀 설정후 PWM 핸들 얻어옴
 pwmA = setPinConfig(ENA, IN1, IN2)
 pwmB = setPinConfig(ENB, IN3, IN4)
 
-GPIO.setup(IN5, GPIO.IN)    
+GPIO.setup(IN5, GPIO.IN)
 #제어 시작
 
 pygame.init()
@@ -115,22 +122,29 @@ while True:
         	GPIO.cleanup()
 		sys.exit()
 	if event.type == KEYUP:
+                #모터 채널 0,속도 80, 멈춤
         	setMotor(CH1, 80, STOP)
+                #모터 채널 1,속도 80, 멈춤
 		setMotor(CH2, 80, STOP)
 		reading = analog_read(0)
+                #전압 값 계산, 몇 V인지
 		voltage = reading * 3.3 / 1024
 		#print("R = %d \t V = %f" %(reading, voltage))
-    
+
     dist = analog_read(0)
     print(dist)
     print("\n")
 
     keys = pygame.key.get_pressed()
+    #방향키 UP 이면 모터 채널 0,속도 100, 앞으로
     if keys[pygame.K_UP]:
 	setMotor(CH1, 100, FORWARD)
+    #방향키 DOWN 이면 모터 채널 0,속도 100, 뒤로
     if keys[pygame.K_DOWN]:
 	setMotor(CH1, 100, BACKWORD)
+    #방향키 LEFT 이면 모터 채널 1,속도 100, 앞으로
     if keys[pygame.K_LEFT]:
 	setMotor(CH2, 100, FORWARD)
+    #방향키 RIGHT 이면 모터 채널 1,속도 100, 뒤로
     if keys[pygame.K_RIGHT]:
 	setMotor(CH2, 100, BACKWORD)
