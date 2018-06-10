6a719b3e7587db04b7e8629cd2eabcd7a40d7ab7
Wsirius <5dimension.jk@gmail.com>
2018-06-10 22:45:26 +0900

    Update and rename test.py to RCDriver.py

--- RCDriver.py
+++ RCDriver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# 라즈베리파이 GPIO 패키지 
+# 라즈베리파이 GPIO 패키지
 import RPi.GPIO as GPIO
 import spidev
 from time import sleep
@@ -39,37 +39,37 @@ IN5 = 17
 
 
 # 핀 설정 함수
-def setPinConfig(EN, INA, INB):        
+def setPinConfig(EN, INA, INB):
     GPIO.setup(EN, GPIO.OUT)
     GPIO.setup(INA, GPIO.OUT)
     GPIO.setup(INB, GPIO.OUT)
-    # 100khz 로 PWM 동작 시킴 
-    pwm = GPIO.PWM(EN, 91000) 
-    # 우선 PWM 멈춤.   
-    pwm.start(0) 
+    # 100khz 로 PWM 동작 시킴
+    pwm = GPIO.PWM(EN, 91000)
+    # 우선 PWM 멈춤.
+    pwm.start(0)
     return pwm
 
 # 모터 제어 함수
 def setMotorContorl(pwm, INA, INB, speed, stat):
 
     #모터 속도 제어 PWM
-    pwm.ChangeDutyCycle(speed)  
-    
+    pwm.ChangeDutyCycle(speed)
+
     if stat == FORWARD:
         GPIO.output(INA, HIGH)
         GPIO.output(INB, LOW)
-        
+
     #뒤로
     elif stat == BACKWORD:
         GPIO.output(INA, LOW)
         GPIO.output(INB, HIGH)
-        
+
     #정지
     elif stat == STOP:
         GPIO.output(INA, LOW)
         GPIO.output(INB, LOW)
 
-        
+
 # 모터 제어함수 간단하게 사용하기 위해 한번더 래핑(감쌈)
 def setMotor(ch, speed, stat):
     if ch == CH1:
@@ -78,9 +78,9 @@ def setMotor(ch, speed, stat):
     else:
         #pwmB는 핀 설정 후 pwm 핸들을 리턴 받은 값이다.
         setMotorContorl(pwmB, IN3, IN4, speed, stat)
-  
 
-# GPIO 모드 설정 
+
+# GPIO 모드 설정
 GPIO.setmode(GPIO.BCM)
 
 spi = spidev.SpiDev()
@@ -94,11 +94,11 @@ def analog_read(channel):
 
 
 #모터 핀 설정
-#핀 설정후 PWM 핸들 얻어옴 
+#핀 설정후 PWM 핸들 얻어옴
 pwmA = setPinConfig(ENA, IN1, IN2)
 pwmB = setPinConfig(ENB, IN3, IN4)
 
-GPIO.setup(IN5, GPIO.IN)    
+GPIO.setup(IN5, GPIO.IN)
 #제어 시작
 
 pygame.init()
@@ -107,7 +107,19 @@ screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('RaspiRobot')
 pygame.mouse.set_visible(0)
 
+autopilot = 0;
 dist = 0
+dista = 0
+
+def auto_pilot(autopilot, dist):
+	if autopilot:
+		setMotor(CH1, 100, FORWARD)
+		if dist < 750 & dist > 600:
+			setMotor(CH2, 100, FORWARD)
+			sleep(10)
+			setMotor(CH2, 100, BACKWARD)
+			sleep(10)
+			setMotor(CH2, 80, STOP)
 
 while True:
     for event in pygame.event.get():
@@ -120,10 +132,42 @@ while True:
 		reading = analog_read(0)
 		voltage = reading * 3.3 / 1024
 		#print("R = %d \t V = %f" %(reading, voltage))
-    
+
     dist = analog_read(0)
-    print(dist)
-    print("\n")
+    dista = analog_read(2)
+    auto_pilot(autopilot, dist)
+
+    if dist > 900:
+	while True:
+		dist = analog_read(0)
+		for event in pygame.event.get():
+        		if event.type == QUIT:
+                		GPIO.cleanup()
+                		sys.exit()
+        		if event.type == KEYUP:
+				if event.key == pygame.K_SPACE:
+					if autopilot:
+						autopilot = 0
+
+					autopilot = 1
+					
+                		setMotor(CH1, 80, STOP)
+                		setMotor(CH2, 80, STOP)
+
+		if dist < 750:
+			setMotor(CH1, 80, STOP)
+			break
+		if dist > 750:
+			keys = pygame.key.get_pressed()
+    			if keys[pygame.K_UP]:
+        			setMotor(CH1, 100, FORWARD)
+    			if keys[pygame.K_DOWN]:
+        			setMotor(CH1, 100, BACKWORD)
+    			if keys[pygame.K_LEFT]:
+        			setMotor(CH2, 100, FORWARD)
+    			if keys[pygame.K_RIGHT]:
+        			setMotor(CH2, 100, BACKWORD)
+
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
